6acd9a6453ddfd0fd9254dbe77242bb1a22a9f4a
LeniNingsih <129382516+LeniNingsih@users.noreply.github.com>
2023-06-01 22:21:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,23 +7,24 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/translate', methods=['POST'])
+@app.route('/translate', methods=['POST', 'GET'])
 def translate():
-    source_text = request.form['source_text']
-    
-    # Simpan source_text ke file
-    with open("data/source.txt", "w") as f:
-        f.write(source_text)
+    if request.method == 'POST':
+        source_text = request.form['source_text']
+        
+        # Simpan source_text ke file
+        with open("data/source.txt", "w") as f:
+            f.write(source_text)
 
-    # Jalankan kode untuk menerjemahkan
-    command = "onmt_translate -model data/model_step_2000.pt -src data/source.txt -output data/pred.txt"
-    subprocess.run(command, shell=True)
+        # Jalankan kode untuk menerjemahkan
+        command = "onmt_translate -model data/model_step_2000.pt -src data/source.txt -output data/pred.txt"
+        subprocess.run(command, shell=True)
 
-    # Baca file pred.txt
-    with open("data/pred.txt", "r") as f:
-        pred_text = f.read()
-    
-    return pred_text
+        # Baca file pred.txt
+        with open("data/pred.txt", "r") as f:
+            pred_text = f.read()
+        
+        return pred_text
 
 if __name__ == '__main__':
     app.run()
