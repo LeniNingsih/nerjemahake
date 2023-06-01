aa02cf49f14e7188fd64357e7755ce6ff1c90f35
LeniNingsih <129382516+LeniNingsih@users.noreply.github.com>
2023-06-01 22:36:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,50 @@
-from flask import Flask, render_template, request
+import streamlit as st
 import subprocess
 
-app = Flask(__name__)
+# judul/title dan icon
+st.set_page_config(page_title="Nerjemahake",
+    initial_sidebar_state="expanded")
+# Header
+st.title("Nerjemahake")
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# Membuat fungsi to translate the text
+def translate(source_text):
+    # menyimpan source_text ke file
+    with open("data/source.txt", "w") as f:
+        f.write(source_text)
 
-@app.route('/translate', methods=['POST', 'GET'])
-def translate():
-    if request.method == 'POST':
-        source_text = request.form['source_text']
-        
-        # Simpan source_text ke file
-        with open("data/source.txt", "w") as f:
-            f.write(source_text)
+    # menjalankan kode untuk menerjemahkan
+    command = "onmt_translate -model data/model_step_2000.pt -src data/source.txt -output data/pred.txt"
+    subprocess.run(command)
 
-        # Jalankan kode untuk menerjemahkan
-        command = "onmt_translate -model data/model_step_2000.pt -src data/source.txt -output data/pred.txt"
-        subprocess.run(command, shell=True)
+    # membaca file pred.txt
+    with open("data/pred.txt", "r") as f:
+        pred_text = f.read()
+    return pred_text
 
-        # Baca file pred.txt
-        with open("data/pred.txt", "r") as f:
-            pred_text = f.read()
-        
-        return pred_text
+# Sidebar
+st.sidebar.markdown("# Nerjemahake: Aplikasi Penerjemaionhan Bahasa Jawa Ngoko - Krama")
+st.sidebar.markdown("Aplikasi ini adalah demo penerjemahan dengan metode Neural Machine Translation menggunakan OpenNMT.")
+st.sidebar.markdown("Masukkan teks yang ingin diterjemahkan pada kotak teks dan klik 'Terjemahkan'.")
+# st.sidebar.markdown("---")
+# st.sidebar.markdown("# Help")
+# st.sidebar.markdown("Jika ada pertanyaan, silahkan kontak saya di purwaningsihleni85@gmail.com.")
 
-if __name__ == '__main__':
-    app.run()
+# form untuk tampilan fungsi
+with st.form("my_form"):
+    source_text = st.text_area("Masukkan kalimat dalam bahasa jawa ngoko", placeholder="Contoh: Nggawa jeruk kuwi!", height=40, max_chars=200)
+    if st.form_submit_button("Terjemahkan"):
+        # buat nampilin icon muter
+        with st.spinner("Sedang proses"):
+            # untuk fungsi memanggil hasil terjemahan dari source_text
+            pred_text = translate(source_text)
+        # Show translated text in alert box
+        st.info(pred_text)
+
+# menyembunyikan menu
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
